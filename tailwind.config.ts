9849b271fb1173bdc98b1eedef9a3ce1fbabd059
bschm0622 <rfcooper95@gmail.com>
2025-01-06 16:32:17 +0000

    add daisy ui support

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +9,50 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      //"cupcake",
+      // "light",
+      // "dark",
+      // "bumblebee",
+      // "emerald",
+      // "corporate",
+      // "synthwave",
+      "retro",
+      // "cyberpunk",
+      // "valentine",
+      // "halloween",
+      // "garden",
+      // "forest",
+      // "aqua",
+      // "lofi",
+      // "pastel",
+      // "fantasy",
+      // "wireframe",
+      // "black",
+      // "luxury",
+      // "dracula",
+      // "cmyk",
+      // "autumn",
+      // "business",
+      // "acid",
+      // "lemonade",
+      // "night",
+      // "coffee",
+      // "winter",
+      // "dim",
+      // "nord",
+      // "sunset",
+    ],
+  },
+};
+
+export default config;
