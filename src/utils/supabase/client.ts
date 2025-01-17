3cb691964df079411fd47c3f8c5e2acc60b57405
bschm0622 <rfcooper95@gmail.com>
2025-01-16 23:36:46 -0500

    user can only see their plans :)

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,5 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr'
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
 export function createClient() {
   // Create a supabase client on the browser with project's credentials
   return createBrowserClient(
@@ -7,3 +10,4 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
+export const supabase = createClient(supabaseUrl, supabaseKey);
